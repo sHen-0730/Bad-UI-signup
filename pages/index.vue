5978dc754f3eb35d14441a3ee4c05f607f2cff51
sHen <103980830+sHen-0730@users.noreply.github.com>
2023-11-27 05:13:05 +0000

    編集

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
   >
     Primary
   </b-button>
+  <p>test</p>
     <NuxtLink to="/demo/test">wakaba<br></NuxtLink>
     <NuxtLink to="/demo/testn">natsuki<br></NuxtLink>
     <NuxtLink to="/demo/testS">soma<br></NuxtLink>
