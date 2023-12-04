3e72c8e0243746ce9f407f3af75d93e8e0591b4e
sHen <103980830+sHen-0730@users.noreply.github.com>
2023-12-04 07:29:02 +0000

    カウントダウンタイマーのコンポーネントを作成

---
+++ components/CountdownTimer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="countdown-timer">
+    <p>{{ minutes }}:{{ displaySeconds }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCountdownTimer } from "~/composables/useCountdownTimer";
+
+// 5分からカウントダウンする
+const { minutes, displaySeconds, stopTimer } = useCountdownTimer(300);
+</script>
