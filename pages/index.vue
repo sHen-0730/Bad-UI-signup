713c8e67f16d1b1eb6717e2aed284a59f3859589
natsu777 <104060670+natsu777@users.noreply.github.com>
2024-01-17 03:24:42 +0000

    削除

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,6 @@
     <Col>
       <img class="w-50" src="@/assets/Logo.jpg">
     </Col>
-    <Col>
-    <p>test</p>
-    <NuxtLink to="/demo/test">wakaba<br></NuxtLink>
-    <NuxtLink to="/demo/testn">natsuki<br></NuxtLink>
-    <NuxtLink to="/demo/testS">soma<br></NuxtLink>
-    </Col>
   </Row>
 </Container>
 </template>
