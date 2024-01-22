0f8f7c65a90bf76bd62f043010da6aada7e72092
sHen <103980830+sHen-0730@users.noreply.github.com>
2024-01-22 16:17:49 +0000

    index.vueのデザインを変更

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
+  <div class="px-5">
     <div>
-      <h2 class="fs-1 p-5 text-md-start lh-1 text-white">UIの悪さを体験してしよう！</h2>
+      <h2 class="fs-1 py-5 text-md-start lh-1 text-white">UIの悪さを体験してしよう！</h2>
     </div>
     <div>
-      <p class="fs-3 p-5 lh-5 text-white">あなたは制限時間内にこのサイトにサインアップすることができるでしょうか？
+      <p class="fs-4 lh-lg text-white">
+      あなたは制限時間内にこのサイトにサインアップすることができるでしょうか？<br />
       直観的に操作しやすいUIのルールにあえて反して、最悪なUIを作りました。
       普段意識しないで利用しているUIの大切さを実感できるでしょう。</p>
     </div>
-    <div>
-      <p class="p-5 text-white">右矢印を押して始めてください</p>
-      <nuxt-link to="/SignUpPage">→</nuxt-link>
+    <Row class="pt-5 d-flex align-items-center">
+      <Col col="9">
+      <p class="fs-3 text-white">右矢印を押して始めてください
+        <nuxt-link to="/SignUpPage" class="link-offset-2 link-underline link-underline-opacity-0 text-white">→</nuxt-link></p>
+    </Col>
+      <Col col="3">
+      <img src="@/assets/logo.jpg" alt="">
+      </Col>
       <nuxt-link to="/CAPTCHAPage"> 確認用リンク</nuxt-link>  
-    </div>
-  </div>
+  </Row>
+   </div>
 </template>
