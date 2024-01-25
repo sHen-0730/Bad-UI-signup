7c7b828a61626ef6b644cabe17f5426bd7d5dbdb
sHen <103980830+sHen-0730@users.noreply.github.com>
2024-01-25 14:37:35 +0000

    パスワード検証関数をcomposablesに移動

---
+++ frontend/composables/validatePassword.ts
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+
+//パスワードの検証関数
+export const validatePassword = (username: string, password: string) => {
+  //パスワードが12文字以上かどうか
+  if (password.length < 12) {
+    return false;
+  }
+  //パスワードにUsernameに使用している文字が含まれているかどうか
+  if (!password.includes(username)) {
+    return false;
+  }
+  //パスワードに英字の小文字、数字、記号のすべてが含まれているかどうか
+  const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).+$/;
+  if (!regex.test(password)) {
+    return false;
+  }
+  //すべてのルールを満たした場合
+  return true;
+};
