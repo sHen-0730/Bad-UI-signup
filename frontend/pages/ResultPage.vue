2cbac3e2d84c84c4d7c730fcda9f845085fe8052
sHen <103980830+sHen-0730@users.noreply.github.com>
2024-02-02 12:51:32 +0000

    ResultPage.vueのデザインを作成

--- frontend/pages/ResultPage.vue
+++ frontend/pages/ResultPage.vue
@@ -1,3 +1,21 @@
 <template>
-    <p>test</p>
+  <div class="px-5">
+    <div>
+      <h2 class="fs-1 py-5 text-md-start lh-1 text-white">制限時間がなくなりました</h2>
+    </div>
+    <div>
+      <p class="fs-3 lh-lg text-white">
+      UIの大切さは伝わったでしょうか。<br />
+      普段利用しているWebサイトの使いやすさ、ストレスのないデザインを改めて見直す機会になればと思います。</p>
+    </div>
+    <div class="mt-5" style="text-align: center;">
+    <nuxt-link to="/"> <b-button
+    button="success"
+    size="lg"
+    padding="3"
+    >
+   始めに戻る</b-button>
+    </nuxt-link>
+    </div>
+   </div>
 </template>
